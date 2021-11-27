c581aa7ad6b952ca319dfc3369e9b99886c01d76
takizee <takizee53@gmail.com>
2021-11-27 16:41:37 +0200

    Ahbal commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import argparse
-_DEFAULT_INPUT_SHEET_TITLE='urls'
 from manage_google_sheets import *
 from build_the_list import *
+
+_DEFAULT_RESULTS_SHEET_NAME = 'results'
+_DEFAULT_URLS_SHEET_NAME = 'urls'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -10,16 +12,43 @@ if __name__ == '__main__':
                     "for each url ."
     )
     parser.add_argument(
-        "-s",
-        "--spreadsheet",
+        "-i",
+        "--input_sheet",
         type=str,
         required=True,
-        default=_DEFAULT_INPUT_SHEET_TITLE,
-        help="The title of the Goolge Spreadsheet that contains the urls of the websites",
+        default=_DEFAULT_URLS_SHEET_NAME,
+        help="The title of the Google Spreadsheet that contains the urls of the websites"
     )
-    args=parser.parse_args()
-    input_sheet_name=args.spreadsheet
-    list_of_urls=import_urls_from_sheet()
+    parser.add_argument(
+        "-o",
+        "--output_sheet",
+        type=str,
+        required=True,
+        default=_DEFAULT_RESULTS_SHEET_NAME,
+        help="The title of the Goolge Spreadsheet that will contain the worksheets for each keyword list",
+    )
+    args = parser.parse_args()
+    input_sheet_name = args.input_sheet
+    output_sheet_name=args.output_sheet
+    try:
+        RESULTS_SHEET = client.open(output_sheet_name)
+    except  SpreadsheetNotFound as ex:
+        print(f'[✘] Could not open The Google sheet of name {output_sheet_name} , Make sure it exists')
+    try:
+        URLS_SHEET = client.open(input_sheet_name)
+    except SpreadsheetNotFound as ex:
+        print(f'[✘] Could not open The Google sheet of name {input_sheet_name} , Make sure it exists', )
+
+    list_of_urls=import_urls_from_sheet(URLS_SHEET)
+    for url in list_of_urls:
+        list_of_keywords=build_the_list_of_keywords(url)
+        dataframe=convert_list_of_keywords_to_dataframe(list_of_keywords)
+        worksheet=create_worksheet_for_url(RESULTS_SHEET,url)
+        export_dataframe_to_worksheet(dataframe,worksheet)
+        
+
+    print('الحمد لله رب العالمين♥')
+    print('Elhamdulillah ♥')
 
 
 
